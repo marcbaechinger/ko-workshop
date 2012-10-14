e9df9d15734e1868752cce17b2f4e8fc1ea7ce30
marcbaechinger <marc.baechinger@gmail.com>
2012-10-14 18:20:23 +0200

    add a filter ko.observable() and  a filteredCredentials  ko.computed
    * add a filter ko.observable() with an empty string
    * add a filteredCredentials as a ko.computed. The implementation creates a filtered array of credentials and returns it

--- workshop/src/app/application-model.js
+++ workshop/src/app/application-model.js
@@ -19,6 +19,24 @@
 			new Credentials(1, "Facebook", "marcbaechinger", "secret"),
 			new Credentials(1, "Twitter", "marc", "verysecret")
 		]);
+		this.filter = ko.observable();
+		this.filteredCredentials = ko.computed(function () {
+			if (!this.filter()) {
+				return this.credentials;
+			}
+			var retVal = [],
+				credentials = this.credentials(),
+				query = this.filter().toLowerCase(),
+				site, username;
+			for (var i = 0; i < credentials.length; i++) {
+				site = credentials[i].site().toLowerCase();
+				username = credentials[i].username().toLowerCase();
+				if (site.indexOf(query) > -1 || username.indexOf(query) > -1) {
+					retVal.push(credentials[i]);
+				}
+			}
+			return retVal;
+		}, this);
 		
 		this.createCredential = function (site) {
 			var credentials = new Credentials(nextId++, site);
